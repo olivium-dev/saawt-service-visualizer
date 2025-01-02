398f0fa4ea5e8b88dc47203598b92f8fd1607572
Ouday Khaled <ouday.khaled@gmail.com>
2025-01-02 11:12:56 +0100

    Update loadControllers.js

--- data/loadControllers.js
+++ data/loadControllers.js
@@ -1,2 +1,12 @@
 window.appendFlows(window.orderController);
 window.appendFlows(window.refundController);
+
+// Wallet Flows
+window.appendFlows(window.walletControllerGetSystemWallet);
+window.appendFlows(window.walletControllerCreateWalletOwner);
+window.appendFlows(window.walletControllerAddWalletToOwner);
+window.appendFlows(window.walletControllerDeactivateWallet);
+window.appendFlows(window.walletControllerForceDeactivateWallet);
+window.appendFlows(window.walletControllerDeactivateWalletHolder);
+window.appendFlows(window.walletControllerForceDeactivateWalletHolder);
+window.appendFlows(window.walletControllerGetHolderWallets);
